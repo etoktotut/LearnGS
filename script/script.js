257a8aa118ec0c1b6983ef6967406d59c44de690
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-01 08:43:32 +0300

    ДЗ к первому уроку

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-alert('1');
+let money, income, addExpenses, deposit, mission; // для const понадобится присваивание, а для let - вроде не обязательно.
+const period = ['1998', '2000'];
+
+alert('Как бы вот!');
+
+console.log(period, income); //должен получить массив и undefined
+
