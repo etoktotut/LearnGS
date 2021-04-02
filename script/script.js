ca8e3122a68635a67931d0badb3c3b029af15f64
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-02 08:04:23 +0300

    Home Task 2

--- script/script.js
+++ script/script.js
@@ -1,7 +1,27 @@
-let money, income, addExpenses, deposit, mission; // для const понадобится присваивание, а для let - вроде не обязательно.
-const period = ['1998', '2000'];
+const money = 60000;
+const income = 'фриланс';
+const addExpenses = 'Налоги, Страховка, Учеба, Хобби';
+const deposit = true;
+const mission = 1000000;
+const period = 12;
 
-alert('Как бы вот!');
+console.log('Type of money: ', typeof (money));
+console.log('Type of income: ', typeof (income));
+console.log('Type of deposit: ', typeof (deposit));
 
-console.log(period, income); //должен получить массив и undefined
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев.');
+console.log('Цель заработать ' + mission + ' рублей.');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+const budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
+
+
+
+
+
+
 
