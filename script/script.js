44697d81ca5780c7efd1d70db2ab5c1601e77723
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-09 10:08:09 +0300

    from 07 to 06

--- script/script.js
+++ script/script.js
@@ -1,89 +1,38 @@
 "use strict";
 
-let isNumber = (n) => {
+const isNumber = (n) => {
     return !isNaN(parseFloat(n)) && isFinite(n);
+    //&& +n > 0 && +n <= 100;
 };
 
-let money = 0;
-
-const start = function () {
-    do {
-        money = prompt('Ваш месячный доход:').trim();
+const startInputNumber = (mess) => {
+    let num1 = prompt(mess);
+    if (num1 === null) {
+        return null;
     }
-    while (!isNumber(money));
-
+    if (!isNumber(+num1)) {
+        return startInputNumber("Введите число!");
+    }
+    return +num1;
 };
 
-start();
+const getGame = (x) => {
+    const game = (mes) => {
+        let num = startInputNumber(mes);
+        if (num === null) {
+            return alert("Игра окончена");
+        }
+        if (num === x) {
+            return alert('Вы угадали');
+        }
+        if (num > x) {
+            return game('Загаданное число больше');
+        }
+        return game('Загаданное число меньше');
 
-let appData = {
-    income: {},
-    addIncome: [],
-    expenses: {},
-    addExpenses: [],
-    budget: 0,
-    deposit: false,
-    mission: 1000000,
-    period: 12,
-    budgetDay: 0,
-    budgetMonth: 0,
-    expensesMonth: 0,
-    asking: function () {
-        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Кино, Вино, Девочки');
-        appData.addExpenses = addExpenses.toLowerCase().split(',');
-        appData.deposit = confirm('Есть ли у вас депозит в банке?');
-        for (let i = 0; i < 2; i++) {
-            let exp;
-            let temp = prompt('Введите обязательную статью расходов?');
-            do {
-                exp = prompt('Во сколько это обойдется?').trim();
-            }
-            while (!isNumber(exp));
-            appData.expenses[temp] = +exp;
-        }
-    },
-    getExpensesMonth: function () {
-        for (let key in appData.expenses) {
-            appData.expensesMonth += appData.expenses[key];
-        }
-        return;
-    },
-
-    getBudget: function () {
-        appData.budgetMonth = appData.budget - appData.expensesMonth;
-        appData.budgetDay = Math.floor(appData.budgetMonth / 30);
-        return;
-    },
-
-    getTargetMonth: function () {
-        return Math.ceil(appData.mission / appData.budgetMonth);
-    },
-
-    getStatusIncome: function () {
-        if (appData.budgetDay >= 1200) {
-            return 'У вас высокий уровень дохода';
-        }
-        else if (appData.budgetDay >= 600) {
-            return 'У вас средний уровень дохода';
-        }
-        else if (appData.budgetDay >= 0) {
-            return 'К сожалению у вас уровень дохода ниже среднего';
-        }
-        else {
-            return 'Что то пошло не так';
-        }
-    },
-
+    };
+    return game;
 };
 
-appData.asking();
-appData.getExpensesMonth();
-appData.budget = +money;
-appData.getBudget();
-
-console.log('Доходы за месяц: ' + appData.budget + ' руб.');
-console.log('Расходы за месяц: ' + appData.expensesMonth + ' руб.');
-console.log((appData.budgetMonth) > 0 ?
-    'Цель будет достигнута за ' + appData.getTargetMonth() + ' мес.' :
-    'Цель не будет достигнута');
-console.log(appData.getStatusIncome());
+const myGame = getGame(18);
+myGame("Введите число от 1 до 100");
